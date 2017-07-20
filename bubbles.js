d770572562f68f8b23d50ad1b1a345a2110a4e96
ethlee4 <ethanlee2020@u.northwestern.edu>
2017-07-19 23:42:10 -0500

    "initialized bubble class"

--- bubbles.js
+++ bubbles.js
@@ -1,12 +1,24 @@
+
+var bubble;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 500);
+  bubble = new Bubble();
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  background(50, 89, 100);
+  bubble.display();
 }
+
+function Bubble() {
+    this.color = random(0,255);
+    this.radius = random(10, 30);
+    this.x = random(width);
+    this.y = random(height);
+
+    this.display = function() {
+        fill(this.color);
+        ellipse(this.x, this.y, this.radius, this.radius);
+    }
+};
