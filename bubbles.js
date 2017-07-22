9cf59dec1681e7e605c91f96f9323f1c96494f3d
ethlee4 <ethanlee2020@u.northwestern.edu>
2017-07-21 19:55:24 -0500

    "created pulsing bigger and smaller"

--- bubbles.js
+++ bubbles.js
@@ -8,7 +8,7 @@ function setup() {
 function draw() {
     background(50, 89, 100);
     bubble.display();
-    bubble.repel();
+    bubble.pulse();
 }
 
 function distance(x1, y1, x2, y2) {
@@ -18,45 +18,35 @@ function distance(x1, y1, x2, y2) {
 }
 
 function Bubble() {
-    this.color = random(0,255);
-    this.radius = random(10, 30);
-    this.x = random(width);
-    this.y = random(height);
-    this.speedX = 0;
-    this.speedY = 0;
-    this.accelerationX = 0;
-    this.accelerationY = 0;
+    this.position = createVector(width/2, height/2);
+    this.color = 0;
+    this.radius = 80;
+    this.growing = true;
 
     this.display = function() {
+        noStroke();
         fill(this.color);
-        ellipse(this.x, this.y, this.radius, this.radius);
+        ellipse(this.position.x, this.position.y, this.radius, this.radius);
     };
 
-    this.repel = function() {
-        // move bubble in opposite direction of the mouse with speed proportional
-        // to distance between mouse and bubble
-        var distanceX = this.x - mouseX;
-        var distanceY = this.y - mouseY;
+    this.pulse = function() {
+        if (this.growing) {
+            this.radius++;
+            this.color++
+            this.display();
+            if (this.radius > 300) {
+                this.growing = false;
+            }
+        }
 
-        this.accelerationX = .5 * 1/distanceX;
-        this.accelerationY = .5 * 1/distanceY;
+        if (!this.growing) {
+            this.radius--;
+            this.color--;
+            this.display;
+            if (this.radius < 50) {
+                this.growing = true;
+            }
+        }
+    }
 
-        this.speedX += this.accelerationX;
-        this.speedY += this.accelerationY;
-
-        this.x += this.speedX;
-        this.y += this.speedY;
-        console.log(this.speedX, this.speedY);
-    };
-
-    this.pop = function() {
-        // pop bubble once it travels a certain distance
-        // pop works by splitting arc of circle into outwards lines in certain interval
-    };
-
-    this.create = function() {
-        // creates a new bubble
-    };
-
-    // create a bubble blower that creates a new bubble if the speed is > certain threshold?
 };
